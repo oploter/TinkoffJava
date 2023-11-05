14c89445f50a9fa62292a65cdf9ceb1414add171
oploter <pndreya@gmail.com>
2023-11-06 00:01:55 +0300

    Project2 add

--- src/test/java/edu/project2/ExtremeTest.java
+++ src/test/java/edu/project2/ExtremeTest.java
@@ -3,6 +3,9 @@ package edu.project2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ExtremeTest {
@@ -48,5 +51,30 @@ public class ExtremeTest {
             generator.generate(11, -2);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Cannot create maze with non-positive dimensions");
+
+        Maze maze = generator.generate(3, 4);
+        assertThat(maze.getWidth()).isEqualTo(4);
+        assertThat(maze.getHeight()).isEqualTo(3);
+        boolean[][] walls = new boolean[][]{
+                new boolean[]{true, true, true, true},
+                new boolean[]{false, true, true, true},
+                new boolean[]{true, false, true, true},
+                new boolean[]{true, true, false, true},
+                new boolean[]{true, true, true, false},
+                new boolean[]{false, false, true, true},
+                new boolean[]{false, true, false, true},
+                new boolean[]{false, true, true, false},
+                new boolean[]{true, false, false, true},
+                new boolean[]{true, false, true, false},
+                new boolean[]{true, true, false, false},
+                new boolean[]{false, false, false, true},
+                new boolean[]{false, false, true, false},
+                new boolean[]{false, false, false, false},
+                new boolean[]{true, false, false, false},
+        };
+        Cell cell1 = maze.at(new Coordinate(1, 2));
+        assertThat(cell1.col()).isEqualTo(2);
+        assertThat(cell1.row()).isEqualTo(1);
+        assertThat(Arrays.stream(walls).filter(a -> Arrays.equals(cell1.walls(), a)).count()).isPositive();
     }
 }
